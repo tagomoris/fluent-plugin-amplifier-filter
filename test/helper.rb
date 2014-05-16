a03886fc30ff5e7b4805d4a9cfe2103b024e7820
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-05-16 15:01:15 +0900

    add verbose output logger

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,9 @@ require 'test/unit'
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'fluent/test'
-unless ENV.has_key?('VERBOSE')
+if ENV.has_key?('VERBOSE')
+  $log = Fluent::Log.new(Fluent::Test::DummyLogDevice.new, Fluent::Log::LEVEL_TRACE)
+else
   nulllogger = Object.new
   nulllogger.instance_eval {|obj|
     def method_missing(method, *args)
